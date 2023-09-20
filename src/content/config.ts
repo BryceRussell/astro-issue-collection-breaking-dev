7b74d512f5e2338fc29f656e38904f702a43c566
First User Name <brycetrussell@gmail.com>
2023-09-20 15:26:11 -0500

    Remove extraneous schema

--- src/content/config.ts
+++ src/content/config.ts
@@ -3,10 +3,7 @@ import { defineCollection, z } from "astro:content";
 const pages = defineCollection({
   type: 'content', 
   schema: z.object({
-    title: z.string().optional(),
-    description: z.string().optional(),
-    pubDate: z.string().optional(),
-    heroImage: z.string().optional(),
+
   }),
 });
 
